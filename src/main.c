b6ac3dbae1d3ca44158447ff934ef85367125f12
KareemZafan <kareemm.zafan@gmail.com>
2024-07-13 13:35:36 +0300

    getting sqrt of x+y

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <math.h> 
 
-int main(void){
 
-printf("Hello Wrold"); 
+int main(void)
+{
 
-return 0;} 
+	printf("Hello Wrold"); 
+	int x = 80; 
+	int y = 100; 
+	printf("square root of(%d+%d) = %d",x,y,sqrt(x+y)); 
+
+return 0;
+
+} 

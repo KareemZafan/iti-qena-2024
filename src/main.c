eae5c52f8ac3f1792c9d59e3e664ecace85c0d9c
KareemZafan <kareemm.zafan@gmail.com>
2024-07-14 14:20:40 +0300

    implement max function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <math.h> 
 
+int max(int a , int b){
+
+   return (a > b) ? a : b; 
+}
 
 int main(void)
 {
@@ -8,6 +12,10 @@ int main(void)
 	int y = 100; 
 	printf("square root of(%d+%d) = %d",x,y,sqrt(x+y)); 
 	printf("Learning Branching Strategy\n"); 
+	
+	
+	// get max 
+	printf("%d",max(20,60)); 
 
 return 0;
 } 

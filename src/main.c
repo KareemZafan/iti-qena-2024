a05e3b615ee93e9746d2ba4cf757f7a24d29dcd5
KareemZafan <kareemm.zafan@gmail.com>
2024-07-14 14:57:20 +0300

    adding Ahmed's task

--- src/main.c
+++ src/main.c
@@ -8,12 +8,17 @@ int max(int a , int b){
    return b ; 
 }
 
+int round (double a){
+  return (int) a; 
+}
+
 int main(void)
 {
 	int x = 80; 
 	int y = 100; 
 	printf("square root of(%d+%d) = %d",x,y,sqrt(x+y)); 
 	printf("Learning Branching Strategy\n"); 
+        printf("%d",round(90.2)); 
 	
 	
 	// get max 
